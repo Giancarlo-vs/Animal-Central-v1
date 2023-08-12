31fb8a5872d1eafc72259f043c45087b6eb0516d
Gian <80838141+Giancvs2021@users.noreply.github.com>
2023-08-12 14:09:45 -0500

    Update schema2.sql
    Information to schema 2 added

--- src/main/resources/schema2.sql
+++ src/main/resources/schema2.sql
@@ -7,3 +7,23 @@ CREATE TABLE IF NOT EXISTS
         telephone VARCHAR(10)
     );
 
+INSERT INTO pet_adoption (name, pet_type,description,telephone)
+VALUES ('Giancarlo', 'cat','I wanna adopt a cat','5550011');
+INSERT INTO pet_adoption (name, pet_type,description,telephone)
+VALUES ('Kelly', 'dog','I wanna adopt a dog please','7550011');
+INSERT INTO pet_adoption (name, pet_type,description,telephone)
+VALUES ('Diana', 'cat','I wanna adopt a cat','8550011');
+INSERT INTO pet_adoption (name, pet_type,description,telephone)
+VALUES ('Camila', 'dog','I wanna adopt a dog','9550011');
+INSERT INTO pet_adoption (name, pet_type,description,telephone)
+VALUES ('Alejandro', 'cat','I wanna adopt a cat','1550011');
+INSERT INTO pet_adoption (name, pet_type,description,telephone)
+VALUES ('Pedro', 'dog','I wanna adopt a dog','2550011');
+INSERT INTO pet_adoption (name, pet_type,description,telephone)
+VALUES ('Pablo', 'cat','I wanna adopt a cat','3550011');
+INSERT INTO pet_adoption (name, pet_type,description,telephone)
+VALUES ('Leon', 'dog','I wanna adopt a dog','4550011');
+INSERT INTO pet_adoption (name, pet_type,description,telephone)
+VALUES ('Jaramillo', 'dog','I wanna adopt a dog','6550011');
+INSERT INTO pet_adoption (name, pet_type,description,telephone)
+VALUES ('Arturo', 'dog','I wanna adopt a dog','3550011');
